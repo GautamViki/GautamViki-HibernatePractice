2d642a2a88390634ec98ade4fc2d48c8d6854d86
GautamViki <vikacse@gmail.com>
2022-10-27 21:47:56 +0530

    embedded

---
+++ src/main/java/org/map/MapDemo.java
@@ -0,0 +1,28 @@
+package org.map;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class MapDemo {
+    public static void main(String[] args) {
+        SessionFactory factory = new Configuration().configure().buildSessionFactory();
+        Question question = new Question();
+        question.setQuestionId(1212);
+        question.setQuestion("What is java?");
+
+        Answer answer = new Answer();
+        answer.setAnswerId(344);
+        answer.setAnswer("programming language");
+        question.setAnswer(answer);
+        answer.setQuestion(question);
+
+        Session session = factory.openSession();
+        session.persist(answer);
+        session.persist(question);
+        session.beginTransaction().commit();
+        session.close();
+        factory.close();
+
+    }
+}
